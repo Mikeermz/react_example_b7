f587962572709dafce57062dfba6efda471ab871
Mikee Ramirez <miguel.ramirez@nowports.com>
2019-09-19 20:03:07 -0500

    create acss with body
    Signed-off-by: Mikee Ramirez <miguel.ramirez@nowports.com>

--- src/components/global/body/index.js
+++ src/components/global/body/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './style.css';
 
 const Body = () => (
-  <a
-    className="App-link"
-    href="https://reactjs.org"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-      Learn React
-  </a> 
+  <div className="App-body">
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+        Learn React
+    </a> 
+    <p>
+      HEY A TODOS
+    </p>
+  </div>
 );
 
 export default Body;
