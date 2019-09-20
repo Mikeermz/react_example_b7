059f3987e6370644de038245171723b57ce5c367
Mikee Ramirez <miguel.ramirez@nowports.com>
2019-09-19 21:07:06 -0500

    Navbar adding and reacstrap
    Signed-off-by: Mikee Ramirez <miguel.ramirez@nowports.com>

--- src/components/global/header/index.js
+++ src/components/global/header/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './style.css';
+import Navbar from '../Navbar'
 
 const Header = () => (
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Hola a todos
-    </p>
-  </header>
+  <Fragment>
+    <Navbar />
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Hola a todos
+      </p>
+    </header>
+  </Fragment>
 );
 
 export default Header;
