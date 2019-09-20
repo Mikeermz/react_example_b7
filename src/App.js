59c2fff0864a588509abdb80c6867a13f464ed74
Mikee Ramirez <miguel.ramirez@nowports.com>
2019-09-19 19:27:46 -0500

    First commit
    Signed-off-by: Mikee Ramirez <miguel.ramirez@nowports.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Header from './components/global/header'
+// import Body from './components/global/body'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    {/* <Body /> */}
+    {/* <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a> */}
+  </div>
+);
 
 export default App;
---
+++ src/components/global/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './style.css';
+
+const Header = () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Hola a todos
+    </p>
+  </header>
+);
+
+export default Header;
