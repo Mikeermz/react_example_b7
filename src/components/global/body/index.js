61d2b676672adf2e1850abe5687a704e69ad2765
Mikee Ramirez <miguel.ramirez@nowports.com>
2019-09-19 21:25:09 -0500

    Adding cards
    Signed-off-by: Mikee Ramirez <miguel.ramirez@nowports.com>

--- src/components/global/body/index.js
+++ src/components/global/body/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
+import MovieCard from '../../Card';
 import './style.css';
 
 const Body = ({propImage, title}) => (
   <div className="App-body">
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-        Learn React
-    </a> 
-    <p>
-      {title}
-    </p>
-    <img src={propImage} className="App-image-logo" alt="logo" /> 
+    <MovieCard />
   </div>
 );
 
